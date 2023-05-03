4296dceca2c48b0ef06432416f104e9319bc2572
qamarayman <132467117+qamarayman@users.noreply.github.com>
2023-05-04 00:26:14 +0300

    Add files via upload

---
+++ m.js
@@ -0,0 +1,11 @@
+let img =document.querySelector('.first') ;
+let all =document.querySelector('.all') ;
+
+function veges (vege){
+    img.src =vege;
+}
+function colors(color){
+    all.style.background =color;
+
+}
+
